61b9f768e388fda9d4d0bd806c67c81a6e568097
James Cuzella <james.cuzella@lyraphase.com>
2025-02-02 14:37:30 -0700

    bootstrap.sh: Suppress shellcheck SC2317 due to false positive
    - This commit should be reverted if koalaman/shellcheck#2660 is resolved

--- bootstrap-scripts/bootstrap.sh
+++ bootstrap-scripts/bootstrap.sh
@@ -121,6 +121,7 @@ prevent_sudo_timeout() {
 }
 
 # Kill sudo timestamp refresh PID and invalidate sudo timestamp
+# shellcheck disable=SC2317 # false-positive koalaman/shellcheck#2660
 kill_timeout_loop() {
   echo "Killing $timeout_loop_PID due to trap"
   kill -TERM $timeout_loop_PID
